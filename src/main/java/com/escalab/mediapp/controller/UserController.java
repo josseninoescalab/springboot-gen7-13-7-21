797ab5f52ec0616bd2749776ad43268f684df3da
Josse <jossenino@desinsoft.com>
2021-08-10 21:41:02 -0400

    Agregando método para buscar por id en UserController

--- src/main/java/com/escalab/mediapp/controller/UserController.java
+++ src/main/java/com/escalab/mediapp/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class UserController {
 	public ResponseEntity<List<Usuario>> listar(){
 		List<Usuario> lista = userService.findAll();
 		return new ResponseEntity<List<Usuario>>(lista, HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> leerPorId(@PathVariable("id") Integer id){
+		Usuario usuario = userService.findById(id);
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
 	
 	@GetMapping("/getAll")
